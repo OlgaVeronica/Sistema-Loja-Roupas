2f5540c6cd6d6ae238f2ff8737b4b187eb998321
VitorSaiter <vitorsaitdsilva2004@gmail.com>
2022-10-02 23:00:07 -0400

    ...

--- LojaRoupa/Views/FinanceiroUC.xaml.cs
+++ LojaRoupa/Views/FinanceiroUC.xaml.cs
@@ -13,7 +13,6 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using LojaRoupa.Views;
-using LojaRoupa.ViewsModels;
 using LojaRoupa.Views.SubViews;
 
 namespace LojaRoupa.Views
