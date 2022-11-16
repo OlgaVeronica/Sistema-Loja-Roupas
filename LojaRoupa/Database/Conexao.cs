29e52e24b137aa80fc696066de8a3ba3d6143200
Maria Eduarda Ribeiro Viana <viana.ma.ribeiro@gmail.com>
2022-11-16 14:08:27 -0400

    banner
    banner

--- LojaRoupa/Database/Conexao.cs
+++ LojaRoupa/Database/Conexao.cs
@@ -12,7 +12,7 @@ namespace LojaRoupa.Database
     {
         private static string host = "localhost";
 
-        private static string port = "3306";
+        private static string port = "3360";
 
         private static string user = "root";
 
