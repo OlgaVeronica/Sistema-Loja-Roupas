ae6260dd0994680af8260258125bdc943b8c97c5
OlgaVeronica <olgavas.opo@gmail.com>
2022-11-24 16:24:25 -0400

    conserto de conflitos

--- LojaRoupa/Views/SubViews/CadastrarPagamento.xaml.cs
+++ LojaRoupa/Views/SubViews/CadastrarPagamento.xaml.cs
@@ -37,6 +37,7 @@ namespace LojaRoupa.Views.SubViews
         {
             carregarListagem();
         }
+
             //VERIFICAÇÃO DE NLO OU VAZIO DE PAGAMENTO
 
             /*if (
@@ -56,19 +57,6 @@ namespace LojaRoupa.Views.SubViews
             {
                 //aaaaaaaaaaaaa
             }*/
-        }
-
-        private void btnVoltar_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-        
-        private void btnPagar_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-        private void btnPesquisar_Click(object sender, RoutedEventArgs e)
-        {
 
         private void btnVoltar_Click(object sender, RoutedEventArgs e)
         {
