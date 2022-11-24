84cfb2db4ffbeadd5dde8768bba0fc1627c5b3e1
OlgaVeronica <olgavas.opo@gmail.com>
2022-11-23 20:55:08 -0400

    atualizações do cadastrar caixa

--- LojaRoupa/Views/SubViews/CadastrarCaixaUC.xaml.cs
+++ LojaRoupa/Views/SubViews/CadastrarCaixaUC.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using LojaRoupa.DAOs;
+using LojaRoupa.ViewsModels;
+
 
 namespace LojaRoupa.Views.SubViews
 {
@@ -21,11 +24,44 @@ namespace LojaRoupa.Views.SubViews
     public partial class CadastrarCaixaUC : UserControl
     {
         public Frame _frame;
+        private CaixaModel _caixaModel = new CaixaModel();
+        
 
         public CadastrarCaixaUC(Frame frame)
         {
             InitializeComponent();
             _frame = frame;
+            Loaded += CadastrarCaixaUC_Loaded;
+        }
+
+        private void CadastrarCaixaUC_Loaded(object sender, RoutedEventArgs e)
+        {
+            txtSaldoInicial.Text = _caixaModel.SaldoInicial.ToString();
+            txtSaldoFinal.Text = _caixaModel.SaldoFinal.ToString();
+            txtTotalEntrada.Text = _caixaModel.TotalEntrada.ToString();
+            txtTotalSaida.Text = _caixaModel.TotalSaida.ToString();
+            txtNumeroCaixa.Text = _caixaModel.Numero.ToString();
+            txtStatus.Text = _caixaModel.Status; 
+        }
+
+        public void VerificarCaixa()
+        {
+
+        }
+
+        private void btnVoltar_Click(object sender, RoutedEventArgs e)
+        {
+            _frame.Content = new CaixaUC(_frame);
+        }
+
+        private void btnAbrirCaixa_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnFecharCaixa_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
