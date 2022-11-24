6e17b1f149166b1d9619800d37534f1a74dfa5bd
VitorSaiter <vitorsaitdsilva2004@gmail.com>
2022-11-24 14:14:14 -0400

    DespesaModel

---
+++ LojaRoupa/ViewsModels/DespesaModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaRoupa.ViewsModels
+{
+    public class DespesaModel
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public string Vencimento { get; set; }
+        public float Valor { get; set; }
+        public string Status { get; set; }
+    }
+}
