97fcc213248f4300e3150f326ac7b623ecb293e2
Gabriel de Oliveira Silva <gabriel.oliveira.s177@gmail.com>
2022-11-23 20:58:02 -0400

    Update CaixaDAO.cs

--- LojaRoupa/DAOs/CaixaDAO.cs
+++ LojaRoupa/DAOs/CaixaDAO.cs
@@ -3,11 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using LojaRoupa.ViewsModels;
 namespace LojaRoupa.DAOs
 {
-    public class CaixaDAO
+    public class CaixaDAO : AbstractDAO<CaixaModel>
     {
+        public override void Delete(CaixaModel t)
+        {
+            throw new NotImplementedException();
+        }
 
+        public override void Insert(CaixaModel t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<CaixaModel> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(CaixaModel t)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
